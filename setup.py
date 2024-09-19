c999ee9eaf258329a538d0ff71dfefad77add18c
Yiyang Lu <y.lu@pdm-solutions.com>
2024-09-19 16:47:28 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pdmInfra",
+    version="0.1.0",
+    packages=find_packages(),
+    url="https://github.com/pdmimpulse/pdmInfra.git",
+    author="Yiyang Lu",
+    author_email="y.lu@pdm-solutions.com",
+    description="pdm impulse team infrastructure for AI application development",
+)
