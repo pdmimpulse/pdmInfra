07df6c32257cef8892596a439426a4817e5772f5
Yiyang Lu <y.lu@pdm-solutions.com>
2024-11-18 18:40:58 +0100

    changed setup.py to correct versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdmInfra",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["pdmInfra", "pdmInfra.*"]),
     url="https://github.com/pdmimpulse/pdmInfra.git",
     author="Yiyang Lu",
