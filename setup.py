cf9a8894eab70367561365447585f3a5daded6f3
Yiyang Lu <y.lu@pdm-solutions.com>
2025-03-13 15:34:39 +0100

    Forgot to stage setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pdmInfra",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(include=["pdmInfra", "pdmInfra.*"]),
     url="https://github.com/pdmimpulse/pdmInfra.git",
     author="Yiyang Lu",
